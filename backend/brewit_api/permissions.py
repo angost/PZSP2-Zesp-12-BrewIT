8a751fc88609cbc80134a14d0ae1b33b9d7eca91
Michal <michal.golawski.mg@gmail.com>
2024-12-29 15:22:24 +0100

    Adjusted permissions to changed in account model

--- backend/brewit_api/permissions.py
+++ backend/brewit_api/permissions.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions
+from .models import Account
 
 class IsProductionBrewery(permissions.BasePermission):
     message = 'You must be a Production Brewery to access this endpoint.'
     def has_permission(self, request, view):
-        return request.user.groups.filter(name='Production_Brewery').exists()
+        return request.user.role == Account.AccountRoles.PRODUCTION.value
