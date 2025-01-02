4846e70e982368cad135ea4325c5f856faf7995e
Michal <michal.golawski.mg@gmail.com>
2025-01-03 00:12:22 +0100

    Added IsBrewery and IsContractBrewery permissions

--- backend/brewit_api/permissions.py
+++ backend/brewit_api/permissions.py
@@ -5,3 +5,14 @@ class IsProductionBrewery(permissions.BasePermission):
     message = 'You must be a Production Brewery to access this endpoint.'
     def has_permission(self, request, view):
         return request.user.role == Account.AccountRoles.PRODUCTION.value
+
+class IsContractBrewery(permissions.BasePermission):
+    message = 'You must be a Contract Brewery to access this endpoint.'
+    def has_permission(self, request, view):
+        return request.user.role == Account.AccountRoles.CONTRACT.value
+
+class IsBrewery(permissions.BasePermission):
+    message = 'You must be a Brewery to access this endpoint.'
+    def has_permission(self, request, view):
+        return request.user.role == Account.AccountRoles.PRODUCTION.value\
+               or request.user.role == Account.AccountRoles.CONTRACT.value
