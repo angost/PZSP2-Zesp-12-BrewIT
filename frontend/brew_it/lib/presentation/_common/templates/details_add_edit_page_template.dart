9d5cd01249df560775ab5f5ca4fb6dafb5cb56db
Angelika Ostrowska <01178572@pw.edu.pl>
2025-01-20 18:59:02 +0100

    Split add edit details view into 2 columns

--- frontend/brew_it/lib/presentation/_common/templates/details_add_edit_page_template.dart
+++ frontend/brew_it/lib/presentation/_common/templates/details_add_edit_page_template.dart
@@ -8,22 +8,21 @@ import 'package:brew_it/injection_container.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-
 class DetailsAddEditPageTemplate extends StatefulWidget {
   DetailsAddEditPageTemplate(
       {required this.title,
-        this.buttons,
-        this.options,
-        required this.fieldNames,
-        required this.jsonFieldNames,
-        this.fieldEditable,
-        this.fieldTypes,
-        this.elementData,
-        this.enumOptions,
-        this.fetchOptions,
-        this.fetchDisplay,
-        this.displayValues,
-        super.key});
+      this.buttons,
+      this.options,
+      required this.fieldNames,
+      required this.jsonFieldNames,
+      this.fieldEditable,
+      this.fieldTypes,
+      this.elementData,
+      this.enumOptions,
+      this.fetchOptions,
+      this.fetchDisplay,
+      this.displayValues,
+      super.key});
 
   final String title;
   final List<MainButton>? buttons;
@@ -64,6 +63,7 @@ class _DetailsAddEditPageTemplateState
     }
     elementData = widget.elementData ?? {};
   }
+
   @override
   Widget build(BuildContext context) {
     List<String>? fieldValues;
@@ -72,7 +72,7 @@ class _DetailsAddEditPageTemplateState
     if (widget.elementData != null && widget.elementData!.isNotEmpty) {
       fieldValues = List.generate(
           widget.jsonFieldNames.length,
-              (index) => widget.elementData![widget.jsonFieldNames[index]] != null
+          (index) => widget.elementData![widget.jsonFieldNames[index]] != null
               ? widget.elementData![widget.jsonFieldNames[index]].toString()
               : "");
     }
@@ -85,6 +85,9 @@ class _DetailsAddEditPageTemplateState
         }
       }
     }
+
+    final fieldsWidgets = generateFieldsWidgets(fieldValues);
+    final splitIndex = (fieldsWidgets.length / 2).round();
 
     return Scaffold(
         appBar: MyAppBar(context),
@@ -125,142 +128,161 @@ class _DetailsAddEditPageTemplateState
                       flex: 8,
                       child: Form(
                           key: formKey,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: List.generate(widget.fieldNames.length, (index) {
-                              bool editable = widget.fieldEditable != null && widget.fieldEditable![index];
-                              String fieldType = widget.fieldTypes != null ? widget.fieldTypes![index] : "TextField";
-                              String jsonFieldName = widget.jsonFieldNames[index];
-
-                              switch (fieldType) {
-                                case "DatePickerField":
-                                  return Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                                    child: DatePickerField(
-                                      label: widget.fieldNames[index],
-                                      jsonFieldName: jsonFieldName,
-                                      initialValue: fieldValues != null && fieldValues[index].isNotEmpty
-                                          ? fieldValues[index]
-                                          : "",
-                                      editable: editable,
-                                      onSaved: (newValue) {
-                                        if (editable) {
-                                          widget.elementData ??= {};
-                                          widget.elementData![jsonFieldName] = newValue;
-                                        }
-                                      },
-                                    ),
-                                  );
-                                case "EnumField":
-                                  return Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                                    child: EnumField(
-                                      label: widget.fieldNames[index],
-                                      jsonFieldName: jsonFieldName,
-                                      options: widget.enumOptions?[jsonFieldName] ?? [],
-                                      selectedValue: fieldValues != null
-                                          ? fieldValues[index]
-                                          : "",
-                                      editable: editable,
-                                      onChanged: (newValue) {
-                                        if (editable) {
-                                          widget.elementData ??= {};
-                                          widget.elementData![jsonFieldName] = newValue;
-                                        }
-                                      },
-                                    ),
-                                  );
-                                case "BooleanField":
-                                  widget.elementData![jsonFieldName] = fieldValues != null ? (fieldValues[index]) : "false";
-                                  return Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                                    child: BooleanField(
-                                      label: widget.fieldNames[index],
-                                      jsonFieldName: jsonFieldName,
-                                      value: fieldValues != null
-                                          ? (fieldValues[index] == "true") // Convert to boolean
-                                          : false,
-                                      editable: editable,
-                                      onChanged: (newValue) {
-                                        if (editable) {
-                                          widget.elementData ??= {};
-                                          widget.elementData![jsonFieldName] = newValue.toString();
-                                          fieldValues != null ? fieldValues[index] = newValue.toString() : false; // Update fieldValues
-                                          setState(() {}); // Trigger rebuild
-                                        }
-                                      },
-                                    ),
-                                  );
-                                case "DisplayField":
-                                  return Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                                    child: DisplayField(
-                                      label: widget.fieldNames[index],
-                                      displayValue: widget.displayValues?[jsonFieldName] ?? "",
-                                      formValue: fieldValues != null
-                                          ? fieldValues[index]
-                                          : "",
-                                    ),
-                                  );
-
-                                default: // TextField
-                                  return Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                                    child: TextFormField(
-                                      onSaved: (newValue) {
-                                        if (editable) {
-                                          widget.elementData ??= {};
-                                          widget.elementData![jsonFieldName] = newValue;
-                                        }
-                                      },
-                                      decoration: InputDecoration(
-                                        labelText: widget.fieldNames[index],
-                                        border: editable
-                                            ? null
-                                            : disabledTextFormFieldTheme.border,
-                                        fillColor: editable
-                                            ? null
-                                            : disabledTextFormFieldTheme.fillColor,
-                                      ),
-                                      initialValue: fieldValues != null
-                                          ? fieldValues[index]
-                                          : "",
-                                      enabled: editable,
-                                      style: baseTextTheme.bodyLarge,
-                                    ),
-                                  );
-                              }
-                            }),
-                          ),
-                      ))
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children:
+                                      fieldsWidgets.sublist(0, splitIndex),
+                                ),
+                              ),
+                              const SizedBox(
+                                width: 100,
+                              ),
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: fieldsWidgets.sublist(
+                                      splitIndex, fieldsWidgets.length),
+                                ),
+                              ),
+                            ],
+                          )))
                 ],
               ),
             ],
           ),
         ));
   }
+
+  List<Widget> generateFieldsWidgets(List<String>? fieldValues) {
+    return List.generate(widget.fieldNames.length, (index) {
+      bool editable =
+          widget.fieldEditable != null && widget.fieldEditable![index];
+      String fieldType =
+          widget.fieldTypes != null ? widget.fieldTypes![index] : "TextField";
+      String jsonFieldName = widget.jsonFieldNames[index];
+
+      switch (fieldType) {
+        case "DatePickerField":
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: DatePickerField(
+              label: widget.fieldNames[index],
+              jsonFieldName: jsonFieldName,
+              initialValue: fieldValues != null && fieldValues[index].isNotEmpty
+                  ? fieldValues[index]
+                  : "",
+              editable: editable,
+              onSaved: (newValue) {
+                if (editable) {
+                  widget.elementData ??= {};
+                  widget.elementData![jsonFieldName] = newValue;
+                }
+              },
+            ),
+          );
+        case "EnumField":
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: EnumField(
+              label: widget.fieldNames[index],
+              jsonFieldName: jsonFieldName,
+              options: widget.enumOptions?[jsonFieldName] ?? [],
+              selectedValue: fieldValues != null ? fieldValues[index] : "",
+              editable: editable,
+              onChanged: (newValue) {
+                if (editable) {
+                  widget.elementData ??= {};
+                  widget.elementData![jsonFieldName] = newValue;
+                }
+              },
+            ),
+          );
+        case "BooleanField":
+          widget.elementData![jsonFieldName] =
+              fieldValues != null ? (fieldValues[index]) : "false";
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: BooleanField(
+              label: widget.fieldNames[index],
+              jsonFieldName: jsonFieldName,
+              value: fieldValues != null
+                  ? (fieldValues[index] == "true") // Convert to boolean
+                  : false,
+              editable: editable,
+              onChanged: (newValue) {
+                if (editable) {
+                  widget.elementData ??= {};
+                  widget.elementData![jsonFieldName] = newValue.toString();
+                  fieldValues != null
+                      ? fieldValues[index] = newValue.toString()
+                      : false; // Update fieldValues
+                  setState(() {}); // Trigger rebuild
+                }
+              },
+            ),
+          );
+        case "DisplayField":
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: DisplayField(
+              label: widget.fieldNames[index],
+              displayValue: widget.displayValues?[jsonFieldName] ?? "",
+              formValue: fieldValues != null ? fieldValues[index] : "",
+            ),
+          );
+
+        default: // TextField
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: TextFormField(
+              onSaved: (newValue) {
+                if (editable) {
+                  widget.elementData ??= {};
+                  widget.elementData![jsonFieldName] = newValue;
+                }
+              },
+              decoration: InputDecoration(
+                labelText: widget.fieldNames[index],
+                border: editable ? null : disabledTextFormFieldTheme.border,
+                fillColor:
+                    editable ? null : disabledTextFormFieldTheme.fillColor,
+              ),
+              initialValue: fieldValues != null ? fieldValues[index] : "",
+              enabled: editable,
+              style: baseTextTheme.bodyLarge,
+            ),
+          );
+      }
+    });
+  }
+
   Future<void> fetchObjectOptions(Map<String, String> config) async {
     try {
       final response = await getIt<Dio>().get(config['endpoint']!);
       if (response.statusCode == 200) {
         final options = (response.data as List)
             .map((item) => {
-          "display": item[config['displayField']!].toString(),
-          "apiValue": item[config['apiValueField']!].toString(),
-        })
+                  "display": item[config['displayField']!].toString(),
+                  "apiValue": item[config['apiValueField']!].toString(),
+                })
             .toList();
         setState(() {
           widget.enumOptions?[config['enumKey']!] = options;
         });
       }
     } catch (e) {
-      print('Error fetching ${config['enumKey']} options from ${config['endpoint']}: $e');
+      print(
+          'Error fetching ${config['enumKey']} options from ${config['endpoint']}: $e');
     }
   }
 
-  Future<void> fetchFieldDisplayValue({
-    required Map<String, String> config
-  }) async {
+  Future<void> fetchFieldDisplayValue(
+      {required Map<String, String> config}) async {
     String endpoint = config['endpoint']!;
     String fieldKey = config['fieldKey']!;
     String apiValue = config['apiValue']!;
@@ -279,13 +301,11 @@ class _DetailsAddEditPageTemplateState
           print("Display field '$displayField' not found in response data.");
         }
       } else {
-        print('Failed to fetch display value for $fieldKey. Status code: ${response.statusCode}');
+        print(
+            'Failed to fetch display value for $fieldKey. Status code: ${response.statusCode}');
       }
     } catch (e) {
       print('Error fetching display value for $fieldKey: $e');
     }
   }
-
 }
-
-
